01c7599641d935ab12e2b360fbb438d3983a920f
Oriol Bover Vila <bover73@gmail.com>
2024-04-11 11:14:02 +0200

    create functionality added

---
+++ app/lib/actions.ts
@@ -0,0 +1,38 @@
+'use server';
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.number(),
+  status: z.enum(['paid', 'pending']),
+  date: z.string(),
+});
+
+const parseAsInt = (value: string) => parseInt(value, 10);
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true }).extend({
+  amount: z.string().transform(parseAsInt),
+});
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = Math.round(amount * 100);
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
